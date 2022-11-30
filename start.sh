356d02a7ae51e644b417bf7a22625c3e4c3228fc
Giorgia Fiscaletti <giorgia.fiscaletti@mia-platform.eu>
2022-11-30 10:10:33 +0100

    feat: add loki configuration

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 kind create cluster --config utils/kind-config.yaml --kubeconfig ~/.kube/config || exit 1
-[ "$(kubectl config current-context)" != "kind-kindergarten" ] && exit 1
+[ "$(kubectl config current-context)" != "kind-kindred" ] && exit 1
 
-kubectl create namespace kindergarten
+kubectl create namespace kindred
 
 while getopts m: flag
 do
@@ -15,14 +15,16 @@ done
 if [ $mode = "minimal" ] 
 then
     echo "Deploying minimal Envoy configuration..."
-    kubectl apply -f envoy-proxy/minimal -n kindergarten
+    kubectl apply -f envoy-proxy/minimal -n kindred
 else
     echo "Deploying standard Envoy configuration..."
-    kubectl apply -f envoy-proxy/standard -n kindergarten
+    kubectl apply -f envoy-proxy/standard -n kindred
 fi
 
-kubectl apply -f envoy-proxy/envoy.deployment.yaml -n kindergarten
-kubectl apply -f envoy-proxy/envoy.svc.yaml -n kindergarten
-kubectl apply -f echo-server -n kindergarten
+kubectl apply -f envoy-proxy/envoy.deployment.yaml -n kindred
+kubectl apply -f envoy-proxy/envoy.svc.yaml -n kindred
+kubectl apply -f echo-server -n kindred
 
-kubectl ns kindergarten
+helm install --namespace loki --create-namespace loki charts/loki -f charts/loki/values.yaml
+
+kubectl ns kindred
